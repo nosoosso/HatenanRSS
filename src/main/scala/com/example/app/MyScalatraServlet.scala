1a532d8206026a2468b96b12bb9bcb9860470183
nosoosso <nosoosso.null@gmail.com>
2015-05-11 10:37:13 +0900

    Unicode文字を含むURLのリクエストを処理できなかったのを修正

--- src/main/scala/com/example/app/MyScalatraServlet.scala
+++ src/main/scala/com/example/app/MyScalatraServlet.scala
@@ -1,9 +1,11 @@
 package com.example.app
 
 import java.net.URL
+import java.nio.charset.Charset
 
 import org.scalatra._
 import org.scalatra.scalate.ScalateSupport
+import org.scalatra.util.RicherString.stringToRicherString
 
 class MyScalatraServlet extends HatenanrssStack with ScalateSupport {
 
@@ -16,11 +18,11 @@ class MyScalatraServlet extends HatenanrssStack with ScalateSupport {
       case None => redirect(url("/"))
       case Some(rssUrl) =>
         try {
-          val contents = xml.XML.load(new URL(rssUrl))
-
+          val encodedUrl = rssUrl.formEncode(Charset.forName("UTF-8"))
+          val contents = xml.XML.load(new URL(encodedUrl))
           val fixedContents = XmlUtil.fixLinkTag(contents)
 
-          Ok(fixedContents,Map("Content-type" -> "text/xml"))
+          Ok(fixedContents, Map("Content-type" -> "text/xml"))
         } catch {
           case e: Throwable => halt(400, "Error:\n" + e.getMessage)
         }
